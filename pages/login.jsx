663216f15388c0f3a229feef8eb05e2a007d9760
Jason Enciso <Jencis275@west-mec.local>
2022-03-28 13:12:53 -0700

    started signup and login

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import axios from "axios"
+import Cookies from "js-cookie"
+import React, {useState, useEffect } from "react"
+import { Button, Divider, Form, Message, Segment } from "semantic-ui-react"
+
+
 
 const login = () => {
   return (
-    <div>login</div>
+<>
+
+
+
+
+
+
+
+</>    
   )
 }
 
