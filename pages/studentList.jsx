429fe3b43d80beeba1a24def9f52bbf0e9a74428
william reid gill <blueyeboyreid@gmail.com>
2022-03-28 15:27:54 -0700

    3/28/22

---
+++ pages/studentList.jsx
@@ -0,0 +1,18 @@
+import { Grid, Image, Divider, Container, Loader } from "semantic-ui-react";
+
+import React from "react";
+
+export const studentList = () => {
+  return (
+    <>
+      <Grid columns="four" divided>
+        <Grid.Row>
+          <Grid.Column>name</Grid.Column>
+          <Grid.Column>teacher</Grid.Column>
+          <Grid.Column>date create</Grid.Column>
+          <Grid.Column>date upload</Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </>
+  );
+};
