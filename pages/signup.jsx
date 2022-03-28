3d20005feedacd58e439fcafa8b3c71ff5464e25
Jason Enciso <Jencis275@west-mec.local>
2022-03-28 15:26:23 -0700

    worked on login and signup

--- pages/signup.jsx
+++ pages/signup.jsx
@@ -4,13 +4,63 @@ import { Button, Divider, Form, Message, Segment } from "semantic-ui-react";
 const signup = () => {
   return (
     <>
-     <Form>
-    <Form.Input>
-      
-    </Form.Input>
+      <Form>
+        <Segment>
+          <Form.Input
+            label="firstName"
+            required
+            placeholder="firstName"
+            name="firstName"
+            value={name}
+          ></Form.Input>
 
+          <Form.Input
+            label="lastName"
+            required
+            placeholder="lastName"
+            name="lastName"
+            value={name}
+          ></Form.Input>
 
-     </Form>
+          <Form.input
+            label="email"
+            required
+            placeholder="email"
+            name="email"
+            value={email}
+            type="email"
+          ></Form.input>
+
+          <Form.Input
+            label="password"
+            required
+            placeholder="password"
+            name="password"
+            value={password}
+          ></Form.Input>
+
+          <Form.Input
+            label="class"
+            required
+            placeholder="class"
+            name="class"
+          ></Form.Input>
+
+          <Form.Input
+            label="teacherCode"
+            required
+            placeholder="teacherCode"
+            name="teacherCode"
+          ></Form.Input>
+
+          <Form.Input
+            label="session"
+            required
+            placeholder="session"
+            name="session"
+          ></Form.Input>
+        </Segment>
+      </Form>
     </>
   );
 };
