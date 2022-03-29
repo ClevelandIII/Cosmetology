93c08d43d0d8e299af23cf5bb741763f832a2644
william reid gill <blueyeboyreid@gmail.com>
2022-03-29 15:28:14 -0700

    3/29/22

--- pages/studentList.jsx
+++ pages/studentList.jsx
@@ -2,15 +2,43 @@ import { Grid, Image, Divider, Container, Loader } from "semantic-ui-react";
 
 import React from "react";
 
+
 const studentList = () => {
   return (
     <>
       <Grid columns="four" divided>
-        <Grid.Row>
-          <Grid.Column>name</Grid.Column>
-          <Grid.Column>teacher</Grid.Column>
-          <Grid.Column>date create</Grid.Column>
-          <Grid.Column>date upload</Grid.Column>
+        <Grid.Row style={{
+            borderBottom: "2px solid navy",
+            // height: "35rem",
+            background: "orange",
+            color: "white",
+            justifyContent: "space-evenly"
+          }}>
+          <Grid.Column style={{background:"white", color:"black", width:"20rem", borderRight:"1rem solid orange", textAlign:"center"}}>name</Grid.Column>
+          <Grid.Column style={{background:"white", color:"black", width:"20rem", borderRight:"1rem solid orange", textAlign:"center"}}>teacher</Grid.Column>
+          <Grid.Column style={{background:"white", color:"black", width:"20rem", borderRight:"1rem solid orange", textAlign:"center"}}>date create</Grid.Column>
+          <Grid.Column style={{background:"white", color:"black", width:"20rem", borderRight:"1rem solid orange", textAlign:"center"}}>date upload</Grid.Column>
+        </Grid.Row>
+        
+        <Grid.Row style={{
+            // borderLeft: "20px solid navy",
+            // borderRight: "20px solid navy",
+            height: "9rem",
+            background: "orange",
+            color: "white",
+            textAlign: "center",
+            // justifyContent: "center"
+          }}>
+          <Grid.Column>beep bop</Grid.Column>
+          <Grid.Column>ms.skddo bep</Grid.Column>
+          <Grid.Column>4/20/69</Grid.Column>
+          <Grid.Column>2/4/70</Grid.Column>
+
+          <Grid.Column>beep bop</Grid.Column>
+          <Grid.Column>ms.skddo bep</Grid.Column>
+          <Grid.Column>4/20/69</Grid.Column>
+          <Grid.Column>2/4/70</Grid.Column>
+
         </Grid.Row>
       </Grid>
     </>
