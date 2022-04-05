796f5ca66d8fab7c9b3e47299b3e3fdb0dbd2eca
william reid gill <blueyeboyreid@gmail.com>
2022-04-05 15:21:04 -0700

    4/5/22

--- pages/studentList.jsx
+++ pages/studentList.jsx
@@ -16,100 +16,187 @@ const studentList = () => {
 
         <Grid.Row >
           <Grid.Column className="column">
-            <Segment>Student</Segment>
+            <Segment className="yearSemesterCenter">Student</Segment>
           </Grid.Column>
           <Grid.Column className="column">
-            <Segment>Teacher</Segment>
+            <Segment className="yearSemesterCenter">Teacher</Segment>
           </Grid.Column>
           <Grid.Column className="column">
-            <Segment>Year</Segment>
+            <Segment className="yearSemesterCenter">Year</Segment>
           </Grid.Column>
           <Grid.Column className="column">
-            <Segment>Semester</Segment>
+            <Segment className="yearSemesterCenter">Semester</Segment>
           </Grid.Column>
         </Grid.Row>
 
         <Grid.Row className=" studentRow">
           <Grid.Column className="studentColumn">
-            <Segment>
-              <Image src="https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg" />{" "}
-            first last
+            <Segment style={{width: "110%"}}>
+              <Image className="studentListPic" src="https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg" />{""}
+              <p className="studentListName" style={{width: "80%"}}>William Gill</p>
+            </Segment>
+          </Grid.Column>
+          <Grid.Column className="studentColumn">
+            <Segment style={{width: "110%"}}>
+              <Image className="studentListPic" src="https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg" />{" "}
+              <p className="studentListName"style={{width: "80%"}}>Abdi Romero</p>
+            </Segment>
+          </Grid.Column>
+          <Grid.Column className="studentColumn">
+            <Segment className="yearSemesterCenter" >2nd year</Segment>
+          </Grid.Column>
+          <Grid.Column className="studentColumn">
+            <Segment className="yearSemesterCenter">3rd Semester</Segment>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row className=" studentRow">
+          <Grid.Column className="studentColumn">
+            <Segment style={{width: "110%"}}>
+              <Image className="studentListPic" src="https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg" />{""}
+              <p className="studentListName" style={{width: "80%"}}>Cleveland Williams</p>
             </Segment>
           </Grid.Column>
 
           <Grid.Column className="studentColumn">
-            <Segment>
-              <Image src="https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg" />{" "}
-              first last
+            <Segment style={{width: "110%"}}>
+              <Image className="studentListPic" src="https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg" />{" "}
+              <p className="studentListName"style={{width: "80%"}}>Daniel Nijdl</p>
             </Segment>
           </Grid.Column>
           <Grid.Column className="studentColumn">
-            <Segment>1st year</Segment>
+            <Segment className="yearSemesterCenter" >2nd year</Segment>
           </Grid.Column>
           <Grid.Column className="studentColumn">
-            <Segment>1st Semester</Segment>
+            <Segment className="yearSemesterCenter">2nd Semester</Segment>
           </Grid.Column>
         </Grid.Row>
-        <Grid.Row className="studentRow">
+        <Grid.Row className=" studentRow">
           <Grid.Column className="studentColumn">
-            <Segment>
-              <Image src="https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg" />{" "}
-              first last
+            <Segment style={{width: "110%"}}>
+              <Image className="studentListPic" src="https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg" />{""}
+              <p className="studentListName" style={{width: "80%"}}>Jason Enciso</p>
             </Segment>
-          </Grid.Column >
+          </Grid.Column>
+
           <Grid.Column className="studentColumn">
-            <Segment>
-              <Image src="https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg" />{" "}
-              first last
+            <Segment style={{width: "110%"}}>
+              <Image className="studentListPic" src="https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg" />{" "}
+              <p className="studentListName"style={{width: "80%"}}>Rando Filler</p>
             </Segment>
-          </Grid.Column >
-          <Grid.Column className="studentColumn">
-            <Segment>1st year</Segment>
           </Grid.Column>
           <Grid.Column className="studentColumn">
-            <Segment>1st Semester</Segment>
+            <Segment className="yearSemesterCenter" >1st year</Segment>
+          </Grid.Column>
+          <Grid.Column className="studentColumn">
+            <Segment className="yearSemesterCenter">4th Semester</Segment>
           </Grid.Column>
         </Grid.Row>
-        <Grid.Row className="studentRow">
+        <Grid.Row className=" studentRow">
           <Grid.Column className="studentColumn">
-            <Segment>
-              <Image src="https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg" />{" "}
-              first last
+            <Segment style={{width: "110%"}}>
+              <Image className="studentListPic" src="https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg" />{""}
+              <p className="studentListName" style={{width: "80%"}}>first last</p>
+            </Segment>
+          </Grid.Column>
+
+          <Grid.Column className="studentColumn">
+            <Segment style={{width: "110%"}}>
+              <Image className="studentListPic" src="https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg" />{" "}
+              <p className="studentListName"style={{width: "80%"}}>first last</p>
             </Segment>
           </Grid.Column>
           <Grid.Column className="studentColumn">
-            <Segment>
-              <Image src="https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg" />{" "}
-              first last
-            </Segment>
+            <Segment className="yearSemesterCenter" >1st year</Segment>
           </Grid.Column>
           <Grid.Column className="studentColumn">
-            <Segment>1st year</Segment>
-          </Grid.Column>
-          <Grid.Column className="studentColumn">
-            <Segment>1st Semester</Segment>
+            <Segment className="yearSemesterCenter">1st Semester</Segment>
           </Grid.Column>
         </Grid.Row>
-        <Grid.Row className="studentRow">
+        <Grid.Row className=" studentRow">
           <Grid.Column className="studentColumn">
-            <Segment>
-              <Image src="https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg" />{" "}
-              first last
+            <Segment style={{width: "110%"}}>
+              <Image className="studentListPic" src="https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg" />{""}
+              <p className="studentListName" style={{width: "80%"}}>first last</p>
             </Segment>
           </Grid.Column>
           <Grid.Column className="studentColumn">
-            <Segment>
-              <Image src="https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg" />{" "}
-              first last
+            <Segment style={{width: "110%"}}>
+              <Image className="studentListPic" src="https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg" />{" "}
+              <p className="studentListName"style={{width: "80%"}}>first last</p>
             </Segment>
           </Grid.Column>
           <Grid.Column className="studentColumn">
-            <Segment>1st year</Segment>
+            <Segment className="yearSemesterCenter" >1st year</Segment>
           </Grid.Column>
           <Grid.Column className="studentColumn">
-            <Segment>1st Semester</Segment>
+            <Segment className="yearSemesterCenter">1st Semester</Segment>
           </Grid.Column>
         </Grid.Row>
+        <Grid.Row className=" studentRow">
+          <Grid.Column className="studentColumn">
+            <Segment style={{width: "110%"}}>
+              <Image className="studentListPic" src="https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg" />{""}
+              <p className="studentListName" style={{width: "80%"}}>first last</p>
+            </Segment>
+          </Grid.Column>
+
+          <Grid.Column className="studentColumn">
+            <Segment style={{width: "110%"}}>
+              <Image className="studentListPic" src="https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg" />{" "}
+              <p className="studentListName"style={{width: "80%"}}>first last</p>
+            </Segment>
+          </Grid.Column>
+          <Grid.Column className="studentColumn">
+            <Segment className="yearSemesterCenter" >1st year</Segment>
+          </Grid.Column>
+          <Grid.Column className="studentColumn">
+            <Segment className="yearSemesterCenter">1st Semester</Segment>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row className=" studentRow">
+          <Grid.Column className="studentColumn">
+            <Segment style={{width: "110%"}}>
+              <Image className="studentListPic" src="https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg" />{""}
+              <p className="studentListName" style={{width: "80%"}}>first last</p>
+            </Segment>
+          </Grid.Column>
+
+          <Grid.Column className="studentColumn">
+            <Segment style={{width: "110%"}}>
+              <Image className="studentListPic" src="https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg" />{" "}
+              <p className="studentListName"style={{width: "80%"}}>first last</p>
+            </Segment>
+          </Grid.Column>
+          <Grid.Column className="studentColumn">
+            <Segment className="yearSemesterCenter" >1st year</Segment>
+          </Grid.Column>
+          <Grid.Column className="studentColumn">
+            <Segment className="yearSemesterCenter">1st Semester</Segment>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row className=" studentRow">
+          <Grid.Column className="studentColumn">
+            <Segment style={{width: "110%"}}>
+              <Image className="studentListPic" src="https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg" />{""}
+              <p className="studentListName" style={{width: "80%"}}>first last</p>
+            </Segment>
+          </Grid.Column>
+
+          <Grid.Column className="studentColumn">
+            <Segment style={{width: "110%"}}>
+              <Image className="studentListPic" src="https://t4.ftcdn.net/jpg/02/15/84/43/240_F_215844325_ttX9YiIIyeaR7Ne6EaLLjMAmy4GvPC69.jpg" />{" "}
+              <p className="studentListName"style={{width: "80%"}}>first last</p>
+            </Segment>
+          </Grid.Column>
+          <Grid.Column className="studentColumn">
+            <Segment className="yearSemesterCenter" >1st year</Segment>
+          </Grid.Column>
+          <Grid.Column className="studentColumn">
+            <Segment className="yearSemesterCenter">1st Semester</Segment>
+          </Grid.Column>
+        </Grid.Row>
+        
+        
       </Grid>
     </>
 
